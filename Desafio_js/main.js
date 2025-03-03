1c0e727de8bb4a157d02f83e85dc66fb867ce59e
natanael carvalho dos santos <natanaelc7@gmail.com>
2025-03-03 14:10:40 -0300

    dasafio calculadora IMC

--- Desafio_js/main.js
+++ Desafio_js/main.js
@@ -8,11 +8,27 @@ function calcularIMC() {
     }
 
     const imc = peso / (altura * altura);
+    let classificacao = '';
+
+    if (imc < 18.5) {
+        classificacao = 'Baixo Peso';
+    } else if (imc >= 18.5 && imc < 25) {
+        classificacao = 'Peso Normal';
+    } else if (imc >= 25 && imc < 30) {
+        classificacao = 'Excesso de Peso';
+    } else if (imc >= 30 && imc < 35) {
+        classificacao = 'Obesidade Grau I';
+    } else if (imc >= 35 && imc < 40) {
+        classificacao = 'Obesidade Grau II';
+    } else {
+        classificacao = 'Obesidade Mórbida';
+    }
 
     const result =  document.getElementById('resultado');
+    const classifResult = document.getElementById('classificacao')
 
     result.innerText = `Seu IMC é: ${imc.toFixed(2)}`;
-
+    classifResult.innerText = `Classificação: ${classificacao}`
 
 }
 
